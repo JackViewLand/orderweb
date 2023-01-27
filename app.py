ce8bcf9889af11aa2cfda98905198ae9b8f0fa44
Jack_Huang <pq125uty77jr100@gmail.com>
2023-01-27 20:30:20 +0800

    V2

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask,redirect,render_template,request,session,url_for
+from flask import Flask,redirect,render_template,request,session,url_for,jsonify
 from pymongo import MongoClient
 import pymongo
+from bson.objectid import ObjectId
 app = Flask(__name__)
 app.secret_key='JackViewLand Secret Key'
 
 def get_database():
     CONNECTION_STRING=r'mongodb+srv://admin:admin@cluster0.x24cglf.mongodb.net/?retryWrites=true&w=majority'
     client = MongoClient(CONNECTION_STRING)
-    return client['User_list']
+    return client['restaurant_reservation']
 
 @app.route("/index",methods=['GET','POST'])
 @app.route("/",methods=['GET','POST'])
@@ -18,6 +19,62 @@ def index():
         return render_template('index.html',name=name)
     return render_template('index.html')
 
+@app.route('/user')
+def user():
+    try:
+        name=session['name']
+        print(name)
+        dbname = get_database()
+        collection= dbname["events"]
+        calendar = list(collection.find({'title':{'$regex':f'{name}.*'}}))
+        if len(calendar) != 0:
+            for c in calendar:
+                del c['_id'],c['end_event']
+                return render_template('booking.html',name=name,calendar=calendar)
+        else:
+            return "<a href='/'>無訂位資料!</a>"
+    except:
+            return index()
+
+@app.route('/user/cancel/<id>')
+def cancel(id):
+    dbname = get_database()
+    collection= dbname["events"]
+    collection.delete_one({'id':id})
+    return "<script>alert('cancel the reservation');window.location.replace('/user');</script>"
+
+@app.route('/reserve')
+def reserve():
+    persons=request.args.get('persons')
+    date=request.args.get('date')
+    date=date.split('/')
+    day,month,year=date
+    time=request.args.get('time')
+    t = time.split(' ')[0].split(':')
+    if 'PM' in time:
+        hour,mins=t
+        hour=int(hour)+12
+        start_time=f'{hour}:{mins}:00'
+        end_time=f'{hour+2}:{mins}:00'
+    else:
+        hour,mins=t
+        start_time=f'{hour}:{mins}:00'
+        end_time=f'{hour+2}:{mins}:00'
+    start=f'{year}-{month}-{day} {start_time}'
+    end=f'{year}-{month}-{day} {end_time}'
+    if 'name' in session:
+        name=session['name']
+        title=f'{name} {persons}位'
+    data=[title,start,end]
+    print(data)
+
+    dbname = get_database()
+    collection= dbname["events"]
+    data={'title':title,'start_event':start,'end_event':end,'id':str(title)+str(ObjectId())}
+    collection.insert_one(data)  
+    return "<script>alert('Success');window.location.replace('/');</script>"
+    
+
 @app.route("/signup",methods=['GET','POST'])
 def signup():
     if request.method == 'POST':
@@ -27,7 +84,7 @@ def signup():
         user_data={'email':email,'name':name,'password':password}
         try:
             dbname = get_database()
-            collection= dbname["data"]
+            collection= dbname["user"]
             result=collection.find_one({'email':email})
             if result is None:
                 collection.insert_one(user_data)
@@ -45,7 +102,7 @@ def login():
         email=request.form['email']
         password=request.form['password']
         dbname = get_database()
-        collection= dbname["data"]
+        collection= dbname["user"]
         result=collection.find_one({'email':email})
         if result is not None:
             if result['password'] == password:
@@ -67,6 +124,77 @@ def logout():
 @app.route('/menu')
 def menu():
     return render_template('menu.html')
+
+@app.route('/admin',methods=['GET','POST'])
+def admin():
+    if request.method =='POST':
+        account=request.form['email']
+        password=request.form['password']
+        dbname = get_database()
+        collection= dbname["user"]
+        result=collection.find_one({'account':account})
+        if result is not None:
+            if result['password'] == password:
+                session['admin']=1
+                return admin_calendar()
+            else:
+                content='Passwords do not match!'
+                return 'error'
+    if request.method=='GET':
+        if 'admin' in session:
+            return admin_calendar()
+    return render_template('login.html')
+
+@app.route('/admin/logout')
+def admin_logout():
+    session.pop('admin',None)
+    return redirect(url_for('admin')) 
+
+def admin_calendar():
+    dbname = get_database()
+    collection= dbname["events"]
+    calendar = list(collection.find({}))
+    return render_template('calendar.html', calendar = calendar)
+
+@app.route("/admin/insert",methods=["POST","GET"])
+def insert():
+    if request.method == 'POST':
+        dbname = get_database()
+        collection= dbname["events"]
+        title = request.form['title']
+        start = request.form['start']
+        end = request.form['end']
+        print(title)     
+        print(start)
+        data={'title':title,'start_event':start,'end_event':end,'id':str(title)+str(ObjectId())}
+        collection.insert_one(data)  
+        msg = 'success' 
+    return jsonify(msg)
+
+@app.route("/admin/update",methods=["POST","GET"])
+def update():
+    if request.method == 'POST':
+        dbname = get_database()
+        collection= dbname["events"]
+        title = request.form['title']
+        start = request.form['start']
+        end = request.form['end']
+        id = request.form['id']
+        print(title)     
+        print(start)
+        collection.update_one({'id':id},{'$set':{'title':title,'start_event':start,'end_event':end}})  
+        msg = 'success' 
+    return jsonify(msg)
+
+@app.route("/admin/ajax_delete",methods=["POST","GET"])
+def ajax_delete():
+    if request.method == 'POST':
+        dbname = get_database()
+        collection= dbname["events"]
+        getid = request.form['id']
+        collection.delete_one({'id':getid})
+        msg ='Record deleted successfully' 
+    return jsonify(msg)        
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
--- mongodb_function.py
+++ mongodb_function.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 import pymongo
+from bson.objectid import ObjectId
 
 def get_database():
     CONNECTION_STRING=r'mongodb+srv://admin:admin@cluster0.x24cglf.mongodb.net/?retryWrites=true&w=majority'
@@ -7,18 +8,33 @@ def get_database():
     return client['User_list']
 
 dbname = get_database()
-collection= dbname["data"]
+collection= dbname["test"]
 
 # Insert
 # collection.insert_one({'name':'Jack','scores':100})
-# collection.insert_many([{'name':'Jack','scores':100},{'name':'Jack','scores':90}])
+#collection.insert_many([{'name':'Jack','scores':100},{'name':'May','scores':90}])
 
-name='Ja'
-email='pq125uty77jr100@gmail.com'
-password='1qaz'
-result=collection.find_one({'$or':[{'name':name},{'email':email}]})
-if result is not None:
-    print(result['password'])
-else:
-    print('error')
+#find
+# name='Ja'
+# email='pq125uty77jr100@gmail.com'
+# password='1qaz'
+# result=collection.find_one({'$or':[{'name':name},{'email':email}]})
+# if result is not None:
+#     print(result['password'])
+# else:
+#     print('error')
+
+#update
+#collection.update_many({'name':'May'},{'$set':{'score':200}})
+
+#delete
+# collection.delete_many({'name':'Jack'})
+# id='63d3466950731da87e258387'
+# objInstance=ObjectId(id)
+# cursor=list(collection.find({'_id':objInstance}))
+cursor=list(collection.find({'name':'May'}))
+for i,c in enumerate(cursor):
+    del c['_id'],c['scores']
+    print(i,c)
+print(cursor)
 
